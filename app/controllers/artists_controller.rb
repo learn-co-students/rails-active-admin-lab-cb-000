b5010f1755f78ae81c87953b5e3a12f9fb769c9f
theboymo <billfero@gmail.com>
2018-04-05 14:53:30 +0100

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,50 +1,50 @@
 class ArtistsController < ApplicationController
+  before_action :set_artist, only: [:show, :edit, :update, :delete]
+
   def index
+    @artists = Artist.all
   end
 
   def show
   end
 
-  def new
-    @artist = Artist.new
-  end
+  # def new
+  #   @artist = Artist.new
+  # end
 
-  def create
-    @artist = Artist.new(artist_params)
+  # def create
+  #   @artist = Artist.new(artist_params)
+  #   if @artist.save
+  #     redirect_to @artist
+  #   else
+  #     render :new
+  #   end
+  # end
 
-    if @artist.save
-      redirect_to @artist
-    else
-      render :new
-    end
-  end
+  # def edit
+  # end
 
-  def edit
-    @artist = Artist.find(params[:id])
-  end
+  # def update
+  #   @artist.update(artist_params)
+  #   if @artist.save
+  #     redirect_to @artist
+  #   else
+  #     render :edit
+  #   end
+  # end
 
-  def update
-    @artist = Artist.find(params[:id])
-
-    @artist.update(artist_params)
-
-    if @artist.save
-      redirect_to @artist
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @artist = Artist.find(params[:id])
-    @artist.destroy
-    flash[:notice] = "Artist deleted."
-    redirect_to artists_path
-  end
+  # def destroy
+  #   @artist.destroy
+  #   flash[:notice] = "Artist deleted."
+  #   redirect_to artists_path
+  # end
 
   private
+    def artist_params
+      params.require(:artist).permit(:name)
+    end
 
-  def artist_params
-    params.require(:artist).permit(:name)
-  end
+    def set_artist
+      @artist = Artist.find(params[:id])
+    end
 end
